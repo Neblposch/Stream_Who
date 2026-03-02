b98d3902c2ce70edf9f738f3a62ba341450e9bfc
Annalena <a.see@students.htl-leonding.ac.at>
2026-03-02 16:34:27 +0100

    file management

---
+++ v1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styleA.css">
+</head>
+<body>
+    <header class="header">
+        <img height="50px" src="logo.png" alt="logo">
+    </header>
+</body>
+</html>
