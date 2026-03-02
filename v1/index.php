156aa03d89e9d008e37d6e38e2234cefba31ee8a
Annalena <a.see@students.htl-leonding.ac.at>
2026-03-02 21:28:02 +0100

    v1 landing page progress bzw derweil fertig und ready für anpassungen

--- v1/index.php
+++ v1/index.php
@@ -9,15 +9,39 @@
 <body>
     <header class="header">
         <img height="60vh" src="logo.png" alt="logo">
-        <h2>SteamWho</h2>
+        <h2>StreamWho</h2>
         <p><a href="?">Log In</a></p>
     </header>
+
     <main>
-        <section id="overview">
-            <div id="video">
+        <section id="info">
+            <div id="vid" class="small-info-box">
                 <img src="placeholderVid.png" alt="placeholder">
+            </div>
+            <div id="text" class="small-info-box">
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. 
+                    Quisque faucibus ex sapien vitae pellentesque sem placerat. 
+                    In id cursus mi pretium tellus duis convallis. Tempus leo eu 
+                    aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus 
+                    nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia 
+                    integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+                    sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque 
+                    faucibus ex sapien vitae pellentesque sem placerat. In id cursus 
+                    mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna 
+                    tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. 
+                    Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut 
+                    hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent 
+                    per conubia nostra inceptos himenaeos.</p>
+            </div>
+            <div id="log-in-start">
+                <button><a href="?">Log In and start playing!</a></button>
             </div>
         </section>
     </main>
+
+    <footer>
+        <a href="?">Impressum</a> | <a href="?">Log In</a>
+    </footer>
 </body>
 </html>
