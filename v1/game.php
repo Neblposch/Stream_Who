2a7af24850f51964036da89186f9a0b4a208c841
EmilyHartmann6 <e.hartmann@students.htl-leonding.ac.at>
2026-03-23 15:07:59 +0100

    changed colors

--- v1/game.php
+++ v1/game.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game</title>
 </head>
-<body>
-    
+<body id="gameBody">
+    <div id=""></div>
 </body>
 </html>
