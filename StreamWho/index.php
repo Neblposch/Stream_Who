532bd5300c374b939a403e779d95a1d608d5b03b
annalena3109 <a.see@students.htl-leonding.ac.at>
2026-05-26 13:57:11 +0200

    flipcards

--- StreamWho/index.php
+++ StreamWho/index.php
@@ -48,6 +48,77 @@ startSession();
             </div>
         </section>
         
+        <section id="reviews">
+            <h3>Read our reviews:</h3>
+            <div class="slider">
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Mia (17)</div>
+                    <div class="back">"actually way more fun than i expected 😭 we played it at a sleepover and it got so competitive so fast. like i was SO sure i knew my friends taste in music but i was wrong every round"</div>
+                    </div>
+                </div>
+
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Jonas (19)</div>
+                    <div class="back">“pretty solid party game tbh. connecting spotify was super easy and the whole guessing thing makes it kinda chaotic in a good way. would 100% play again with friends”</div>
+                    </div>
+                </div>
+
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Lena (16)</div>
+                    <div class="back">“this game exposed my friends SO hard 😭 i thought i knew who listens to what but nahh. also it’s really fast paced so no one gets bored”</div>
+                    </div>
+                </div>
+
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Alex (23)</div>
+                    <div class="back">“fun concept and actually works really well in a group. we tried it at a small hangout and it kept everyone involved the whole time. simple but addictive”</div>
+                    </div>
+                </div>
+
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Sofie (19)</div>
+                    <div class="back">“i love how it uses your real spotify stats, it makes everything feel personal. also the reactions when someone gets exposed are priceless 😭”</div>
+                    </div>
+                </div>
+
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">David (20)</div>
+                    <div class="back">“didn’t expect much but it’s actually really entertaining. good mix of guessing and laughing at your friends’ questionable music taste”</div>
+                    </div>
+                </div>
+
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Emma (15)</div>
+                    <div class="back">“we played like 10 rounds and didn’t even notice the time passing. it’s kinda addicting and super easy to understand even if you’ve never played before”</div>
+                    </div>
+                </div>
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Steven (22)</div>
+                    <div class="back">“great for group calls too, we tried it online and it still worked really well. definitely one of the better party games I’ve seen recently”</div>
+                    </div>
+                </div>
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Carol (18)</div>
+                    <div class="back">“THIS GAME IS SO FUN OMG i kept guessing wrong but it didn’t even matter, we were all just laughing the whole time”</div>
+                    </div>
+                </div>
+                <div class="cardflip">
+                    <div class="flip">
+                    <div class="front">Tom (18)</div>
+                    <div class="back">“simple idea but really effective. it turns music taste into a game which is honestly genius for friend groups.”</div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 
     <footer>
